301176eaeef401f5ded9df388172a083b9857078
Yerzhan <yerzhan@yandex-team.ru>
2018-09-09 11:30:02 +0300

    Initial commit

---
+++ operators.js
@@ -0,0 +1,5 @@
+'map' - regular map for stream data
+'filter' - pass only for data with correct conditions
+'debounceTime' - like debounce
+'distinctUntilChanged' - pass only if data is changed
+'throttleTime' - line throttle
